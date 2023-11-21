6eb9a6a0058876a76c52478528b65c843418f5c3
Karim Badwork <karim@badwork.fr>
2023-11-21 15:28:36 +0100

    exo counter test

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Platform } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -7,10 +8,11 @@ import { Button } from "react-native-paper";
 // import { styles } from "./src/styles/styles";
 
 export default function App() {
+	const [count, setCount] = useState(0);
+
 	return (
 		<View style={{ flex: 1, backgroundColor: "pink" }}>
 			<StatusBar style='auto' />
-			<Text>Karim</Text>
 			<SafeAreaView
 				style={[
 					styles.container,
@@ -21,12 +23,49 @@ export default function App() {
 								: Constants.statusBarHeight,
 					},
 				]}>
-				<View style={{ flex: 1, backgroundColor: "pink" }}>
-					<View style={{ flex: 1, backgroundColor: "red" }}>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: "#303030",
+						// flexDirection: "column",
+						// justifyContent: "center",
+						// alignItems: "center",
+					}}>
+					<Text style={styles.title}>{count}</Text>
+					<View style={{ flexDirection: "row" }}>
+						<Button
+							icon='minus'
+							mode='contained'
+							onPress={() => setCount(count - 1)}>
+							Press me
+						</Button>
+						<Button
+							icon='plus'
+							mode='contained'
+							onPress={() => setCount(count + 1)}>
+							Press me
+						</Button>
+					</View>
+					{/* <View style={{ flex: 1, backgroundColor: "red" }}>
 						<Text>Mon texte</Text>
 					</View>
 					<View style={{ flex: 1, backgroundColor: "#FFF" }}></View>
-					<View style={{ flex: 3, backgroundColor: "green" }}></View>
+					<View style={{ flex: 3, backgroundColor: "green" }}></View> */}
+					{/* <View
+						style={[
+							styles.box,
+							{ backgroundColor: "blue" },
+						]}></View>
+					<View
+						style={[
+							styles.box,
+							{ backgroundColor: "green" },
+						]}></View>
+					<View
+						style={[
+							styles.box,
+							{ backgroundColor: "white" },
+						]}></View> */}
 				</View>
 			</SafeAreaView>
 		</View>
@@ -37,6 +76,7 @@ const styles = StyleSheet.create({
 	title: {
 		fontSize: 24,
 		fontWeight: "bold",
+		color: "white",
 	},
 	container: {
 		flex: 1,
@@ -45,5 +85,10 @@ const styles = StyleSheet.create({
 		// alignItems: "center",
 		// justifyContent: "center",
 		// padding: 15,
+	},
+	box: {
+		// height: 100,
+		// width: 100,
+		flex: 1,
 	},
 });
