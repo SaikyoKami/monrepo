4ff53e58799a21ff2fdf5191d719ed2bce5d1bb7
Karim Badwork <karim@badwork.fr>
2023-11-21 14:32:13 +0100

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Constants from "expo-constants";
+import { Button } from "react-native-paper";
+
+// import { styles } from "./src/styles/styles";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={{ flex: 1, backgroundColor: "pink" }}>
+			<StatusBar style='auto' />
+			<Text>Karim</Text>
+			<SafeAreaView
+				style={[
+					styles.container,
+					{
+						paddingTop:
+							Platform.OS != "ios"
+								? 0
+								: Constants.statusBarHeight,
+					},
+				]}>
+				<View style={{ flex: 1, backgroundColor: "pink" }}>
+					<View style={{ flex: 1, backgroundColor: "red" }}>
+						<Text>Mon texte</Text>
+					</View>
+					<View style={{ flex: 1, backgroundColor: "#FFF" }}></View>
+					<View style={{ flex: 3, backgroundColor: "green" }}></View>
+				</View>
+			</SafeAreaView>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+	},
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		// backgroundColor: "#fff",
+		// alignItems: "center",
+		// justifyContent: "center",
+		// padding: 15,
+	},
 });
---
+++ src/styles/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+	},
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
