2087ebb28a89852672388ba16b0e79b8f4e86162
Karim Badwork <karim@badwork.fr>
2023-11-21 16:01:03 +0100

    navigation ok

--- App.js
+++ App.js
@@ -5,10 +5,18 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Constants from "expo-constants";
 import { Button } from "react-native-paper";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./src/screens/HomeScreen";
+import AboutScreen from "./src/screens/AboutScreen";
+
 // import { styles } from "./src/styles/styles";
 
 export default function App() {
 	const [count, setCount] = useState(0);
+
+	const Stack = createNativeStackNavigator();
 
 	return (
 		<View style={{ flex: 1, backgroundColor: "pink" }}>
@@ -23,50 +31,12 @@ export default function App() {
 								: Constants.statusBarHeight,
 					},
 				]}>
-				<View
-					style={{
-						flex: 1,
-						backgroundColor: "#303030",
-						// flexDirection: "column",
-						// justifyContent: "center",
-						// alignItems: "center",
-					}}>
-					<Text style={styles.title}>{count}</Text>
-					<View style={{ flexDirection: "row" }}>
-						<Button
-							icon='minus'
-							mode='contained'
-							onPress={() => setCount(count - 1)}>
-							Press me
-						</Button>
-						<Button
-							icon='plus'
-							mode='contained'
-							onPress={() => setCount(count + 1)}>
-							Press me
-						</Button>
-					</View>
-					{/* <View style={{ flex: 1, backgroundColor: "red" }}>
-						<Text>Mon texte</Text>
-					</View>
-					<View style={{ flex: 1, backgroundColor: "#FFF" }}></View>
-					<View style={{ flex: 3, backgroundColor: "green" }}></View> */}
-					{/* <View
-						style={[
-							styles.box,
-							{ backgroundColor: "blue" },
-						]}></View>
-					<View
-						style={[
-							styles.box,
-							{ backgroundColor: "green" },
-						]}></View>
-					<View
-						style={[
-							styles.box,
-							{ backgroundColor: "white" },
-						]}></View> */}
-				</View>
+				<NavigationContainer>
+					<Stack.Navigator>
+						<Stack.Screen name='Home' component={HomeScreen} />
+						<Stack.Screen name='About' component={AboutScreen} />
+					</Stack.Navigator>
+				</NavigationContainer>
 			</SafeAreaView>
 		</View>
 	);
